0d39b47d9d1b75b882c7d9f6fc890d6fb9bae653
awodesigns-github <awo.designs1@gmail.com>
2023-11-01 12:41:40 +0300

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed
+ *
+ * @argc: the number of arguments passed
+ * @argv: the pointer to an array of pointers to arguments
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
