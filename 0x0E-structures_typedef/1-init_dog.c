93a6f684afdd89f4d1663fce69d759f1a1063376
awodesigns-github <awo.designs1@gmail.com>
2023-11-06 17:30:45 +0300

    init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - returns void
+ * @d: struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		return (NULL);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
