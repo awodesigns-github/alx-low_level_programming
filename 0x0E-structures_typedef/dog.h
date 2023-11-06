66d8b62d723e147006263e46b7cfa8994fc34197
awodesigns-github <awo.designs1@gmail.com>
2023-11-06 12:51:41 +0300

    dog struct header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - structure for a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: structure defining properties for the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
