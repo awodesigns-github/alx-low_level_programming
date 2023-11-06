80e8e613cf659a26247524c18f4469f7abfc6ef9
awodesigns-github <awo.designs1@gmail.com>
2023-11-06 20:18:24 +0300

    print dog file - assigning nil to null elements

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,11 +13,11 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 	{
-	printf("Name: (nil)\n");
+	d->name = "(nil)";
 	}
 	if (d->owner == NULL)
 	{
-	printf("Owner: (nil)\n");
+	d->owner = "(nil)";
 	}
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
