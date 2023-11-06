3b7f652d8842ebb31a97da097bcb536139eaf71f
awodesigns-github <awo.designs1@gmail.com>
2023-11-06 18:22:20 +0300

    init dog - solve pedantic error

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
-#include "dog.h"
 #include <stdlib.h>
+#include "dog.h"
 
 /**
  * init_dog - returns void
@@ -7,14 +7,11 @@
  * @name: name
  * @age: age
  * @owner: owner
- *
- * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
 	if (d == NULL)
-		return (NULL);
+		d = malloc(sizeof(struct dog));
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
