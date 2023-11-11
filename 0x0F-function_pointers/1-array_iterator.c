754c14f7a936ba6f61fbd88cc169437b0cc41853
awodesigns-github <awo.designs1@gmail.com>
2023-11-11 21:22:25 +0300

    array iterator - increment array pointer not array values

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,12 +9,13 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	unsigned int i;
 
-	if (array != NULL && action != NULL)
-		for (i = 0; i < size; i++)
-		{
-		action(*array);
-		*array++;
-		}
+	if (array == NULL || action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+	{
+	action(*array);
+	array++;
+	}
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,7 +2,6 @@
 #define FUNCTION_POINTERS_H
 
 #include <stdlib.h>
-#include <stddef.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
