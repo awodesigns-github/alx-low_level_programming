880ca4271610759d011da89c59dfc5194c27f296
awodesigns-github <awo.designs1@gmail.com>
2023-11-17 11:48:21 +0300

    add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning
+ *
+ * @head: pointer to a pointer to the head node
+ * @str: string to be duplicated
+ *
+ * Return: address of the new element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	new_node->next = *head;
+	new_node->len = strlen(str);
+	*head = new_node;
+	return (*head);
+}
