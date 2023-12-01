d5ef3290a039dd0c66b92ccfbe444528d42faa72
awodesigns-github <awo.designs1@gmail.com>
2023-12-01 14:24:32 +0300

    create file - fix if logic

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,12 +17,11 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_EXCL | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-	if (text_content != NULL)
-	{
-		bytes_w = write(fd, text_content, sizeof(text_content));
-		if (bytes_w == -1)
-			return (-1);
-	}
+	if (text_content == NULL)
+		text_content = "";
+	bytes_w = write(fd, text_content, sizeof(text_content));
+	if (bytes_w == -1)
+		return (-1);
 	close(fd);
 	return (1);
 }
