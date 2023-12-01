08c8bff5c391a7e4c58fbcf66a61eb54692f9356
awodesigns-github <awo.designs1@gmail.com>
2023-12-01 14:27:44 +0300

    create file - use strlen

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		text_content = "";
-	bytes_w = write(fd, text_content, sizeof(text_content));
+	bytes_w = write(fd, text_content, strlen(text_content));
 	if (bytes_w == -1)
 		return (-1);
 	close(fd);
