efa35f7d5bfdce9cc492d7bc1ff075bfc13185e8
awodesigns-github <awo.designs1@gmail.com>
2024-02-14 22:32:23 +0300

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * create_file - function that creates a file
@@ -11,7 +12,21 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
+	ssize_t file_w;
 
 	if (filename == NULL)
 		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	/* write text content to the file */
+	if (!text_content)
+		text_content = "";
+	file_w = write(fd, text_content, strlen(text_content));
+	if (file_w == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
 }
