a5c77dff6842ab2835bacce3639ef7b586b19298
Joseph Mushi <awo.designs1@gmail.com>
2026-02-04 21:36:01 +0300

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - main entry point
+* Return: returns 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
