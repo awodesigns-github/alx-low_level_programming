ca21cb3c625101e395f96a73d52ca4d75b556f01
Joseph Mushi <awo.designs1@gmail.com>
2026-02-04 22:49:25 +0300

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Main entry point
+* Return: returns 0
+*/
+int main(void)
+{
+	int i = 0;
+
+	for (; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
