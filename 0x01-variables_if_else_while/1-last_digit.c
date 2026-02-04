593aa17e928ba3ed4d0171a50354a52b20932754
Joseph Mushi <awo.designs1@gmail.com>
2026-02-04 20:54:53 +0300

    Annoying linter

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,23 +9,22 @@
 int main(void)
 {
 	int n;
-	int last_digit;
+	int lastN;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	/* last digit of n */
-	last_digit = n % 10;
-	if (last_digit > 5)
+	lastN = n % 10;
+	if (lastN > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
-	} else if (last_digit == 0)
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastN);
+	} else if (lastN == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, last_digit);
-	} else if (last_digit < 6 && last_digit != 0)
+		printf("Last digit of %d is %d and is 0\n", n, lastN);
+	} else if (lastN < 6 && lastN != 0)
 	{
-		printf("Last digit of %d is %d and is "
-			"less than 6 and not 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastN);
 	}
 	return (0);
 }
