f5f2080002d5f0023e4123c8026d8a2a2d740a4e
Joseph Mushi <awo.designs1@gmail.com>
2026-02-04 22:52:51 +0300

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Main entry point
+* Return: returns 0
+*/
+int main(void)
+{
+	int i = 48;
+
+	for (; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
